69880622bde0ff33eca6b264f4b86eb60ecbd404
Silver <jairomiguelrodo@gmail.com>
2024-06-09 21:35:41 -0500

    Correccion script.js

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ const chatLog = document.getElementById('chat-log');
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const userInputValue = userInput.value.trim();
-    if (userInputValue !== '') {
-      fetch('/chat', {
+    if (userInputValue!== '') {
+      fetch('http://localhost:5000/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ input: userInputValue })
       })
-      .then(response => response.json())
-      .then((data) => {
-        const responseText = data.response;
-        chatLog.innerHTML += `<p>Usuario: ${userInputValue}</p><p>Bot: ${responseText}</p>`;
-        userInput.value = '';
+     .then(response => response.json())
+     .then((data) => {
+        if (data && data.response) {
+          const responseText = data.response;
+          chatLog.innerHTML += `<p>Usuario: ${userInputValue}</p><p>Bot: ${responseText}</p>`;
+          userInput.value = '';
+        } else {
+          console.error('Error al procesar la respuesta del servidor');
+        }
       })
-      .catch((error) => {
+     .catch((error) => {
         console.error(error);
       });
     }
